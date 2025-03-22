152357f9e1bcbd6b6a7cfbfd9e9a5eb93e63fbe7
rwbxd <will.bryan99@gmail.com>
2025-03-22 11:38:44 -0500

    add format selection

--- server/api.py
+++ server/api.py
@@ -1,5 +1,6 @@
 import subprocess
-from fastapi import FastAPI
+import re
+from fastapi import FastAPI, Response, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -7,18 +8,40 @@ app = FastAPI()
 
 class YTRequest(BaseModel):
     url: str
+    subfolder: str = '/'
+    format: str = 'mp4'
+    res: str = '1080'
 
+allowed_subfolders = ('/')
 
 @app.post("/download")
-def download(request: YTRequest):
-    print("Downloading following URL: " + request.url)
-    return subprocess.run(
-        [f'yt-dlp -o "output/%(title)s.%(ext)s" {request.url}'],
-        capture_output=True,
-        shell=True,
+def download(request: YTRequest, response: Response):
+    request.subfolder = re.sub(r'/+', '/', f"/{request.subfolder}/")
+    if request.subfolder not in allowed_subfolders:
+        response.status_code = 403
+        return 'Subfolder is not on the list of allowed subfolders.'
+    completed_process = subprocess.run(
+        ['yt-dlp',
+         '-o',
+         'output' + request.subfolder + f'%(title)s-%(resolution)s.%(ext)s',
+         '-S',
+         f'ext:{request.format},res:{request.res}',
+         request.url],
+        capture_output=True
     )
+    return completed_process.stdout.decode(errors='replace')
 
+@app.get("/allowed_subfolders")
+def get_allowed_subfolders():
+    return list(allowed_subfolders)
 
-@app.get("/ls")
-def ls(path: str = "."):
-    return subprocess.run([f"ls {path}"], capture_output=True, shell=True)
+@app.get("/list_output")
+def list_output(path: str = ""):
+    path = re.sub(r'/+', '/', f"/{path}/")
+    if path not in allowed_subfolders:
+        raise HTTPException(status_code=403, detail="Subfolder is not on the list of allowed subfolders.")
+    print(f"calling ls for output{path}")
+    try:
+        return subprocess.run(["ls", f"output{path}"], capture_output=True).stdout.decode(errors="replace").split("\n")
+    except:
+        return subprocess.run(["dir", f"output{path.rstrip('/')}", "/b"], capture_output=True, shell=True).stdout.decode(errors="replace").split("\r\n")
