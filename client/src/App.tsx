c99a4f11ada2a3175063dec8dc0e3d01d4c84959
rwbxd <will.bryan99@gmail.com>
2025-03-30 02:14:44 -0500

    slightly improve frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,80 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import { DownloadRequest, listOutput, listSubfolders, sendDownloadRequest } from './api';
+import { useEffect, useState } from "react";
+import "./App.css";
+import {
+  DownloadRequest,
+  getServerOptions,
+  listOutput,
+  sendDownloadRequest,
+} from "./api";
 
 function App() {
   const [files, setFiles] = useState([] as string[]);
   const [subfolders, setSubfolders] = useState([] as string[]);
+  const [resolutions, setResolutions] = useState([] as string[]);
+  const [formats, setFormats] = useState([] as string[]);
 
   async function getFilesInDir() {
     const output = await listOutput();
     setFiles(output);
   }
 
-  useEffect(() => {
-    listSubfolders().then(subfolders => {
-      setSubfolders(subfolders);
+  useEffect(function getInitialData() {
+    getServerOptions().then((serverOptions) => {
+      console.log(serverOptions);
+      setSubfolders(serverOptions.subfolders);
+      setResolutions(serverOptions.resolutions);
+      setFormats(serverOptions.formats);
     });
-  }, [])
+  }, []);
 
   async function submitDownloadRequest(e: any) {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
-    sendDownloadRequest(Object.fromEntries(formData.entries()) as DownloadRequest)
-      .then(res => console.log(res))
+    sendDownloadRequest(
+      Object.fromEntries(formData.entries()) as DownloadRequest,
+    ).then((res) => console.log(res));
     console.log(Object.fromEntries(formData.entries()));
   }
 
   return (
     <>
-      <div>
-        <p>Hello world</p>
+      <div className="app">
+        <h1 className="header">
+          <span className="youtube-header">Youtube</span>2
+          <span className="plex-header">Plex</span>
+        </h1>
         <form onSubmit={submitDownloadRequest}>
           <div>
             <input name="url"></input>
           </div>
           <div>
             <select name="subfolder" hidden={!(subfolders.length > 1)}>
-              {subfolders.map(subfolder => { return <option key="subfolder" value={subfolder}>{subfolder}</option> })}
+              {subfolders.map((subfolder) => {
+                return (
+                  <option key={subfolder} value={subfolder}>
+                    {subfolder}
+                  </option>
+                );
+              })}
             </select>
-            <select name="res">
-              <option value="1080">1080p</option>
-              <option value="720">720p</option>
-              <option value="480">480p</option>
+            <select name="res" hidden={!(resolutions.length > 1)}>
+              {resolutions.map((res) => {
+                return (
+                  <option key={res} value={res}>
+                    {res}
+                  </option>
+                );
+              })}
             </select>
-            <select name="format">
-              <option value="mp4">mp4</option>
-              <option value="m4a">m4a</option>
+            <select name="format" hidden={!(formats.length > 1)}>
+              {formats.map((format) => {
+                return (
+                  <option key={format} value={format}>
+                    {format}
+                  </option>
+                );
+              })}
             </select>
           </div>
           <div>
@@ -53,13 +82,15 @@ function App() {
           </div>
         </form>
         <button onClick={getFilesInDir}>List Files</button>
-        <p>Files array is {files.length ? 'not empty' : 'empty'}</p>
+        <p>Files array is {files.length ? "not empty" : "empty"}</p>
         <ul>
-          {files.map(fileName => { return <li key={fileName}>{fileName}</li> })}
+          {files.map((fileName) => {
+            return <li key={fileName}>{fileName}</li>;
+          })}
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/api.ts
+++ client/src/api.ts
@@ -1,38 +1,57 @@
-const API_URL = "http://localhost:8000"
+const API_URL = "http://localhost:8000";
 
 export async function listOutput(): Promise<string[]> {
-    const response = await fetch(API_URL + "/list_output")
-    const jsonResponse = await response.json();
-    const filteredResponse = jsonResponse.filter((x: string) => x !== '');
-    return filteredResponse as string[]
+  const response = await fetch(API_URL + "/list_output");
+  const jsonResponse = await response.json();
+  const filteredResponse = jsonResponse.filter((x: string) => x !== "");
+  return filteredResponse as string[];
 }
 
 export async function listSubfolders(): Promise<string[]> {
-    const response = await fetch(API_URL + "/allowed_subfolders");
-    const jsonResponse = await response.json();
-    return jsonResponse as string[];
+  const response = await fetch(API_URL + "/allowed_subfolders");
+  const jsonResponse = await response.json();
+  return jsonResponse as string[];
 }
 
 export type DownloadRequest = {
-    url: string;
-    subfolder?: string;
-    res?: string;
-    format?: string;
+  url: string;
+  subfolder?: string;
+  res?: string;
+  format?: string;
+};
+
+export async function sendDownloadRequest(
+  req: DownloadRequest,
+): Promise<boolean> {
+  console.log(JSON.stringify(req));
+  try {
+    const response = await fetch(API_URL + "/download", {
+      method: "POST",
+      body: JSON.stringify(req),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.ok;
+  } catch {
+    console.log("failed");
+    return false;
+  }
 }
 
-export async function sendDownloadRequest(req: DownloadRequest): Promise<boolean> {
-    console.log(JSON.stringify(req));
-    try {
-        const response = await fetch(API_URL + "/download", {
-            method: "POST",
-            body: JSON.stringify(req),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-        return response.ok;
-    } catch {
-        console.log("failed");
-        return false;
-    }
+export type ServerOptions = {
+  subfolders: string[];
+  resolutions: string[];
+  formats: string[];
+};
+
+export async function getServerOptions(): Promise<ServerOptions> {
+  try {
+    const response = await fetch(API_URL + "/allowed_options");
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  } catch {
+    console.log("failed");
+    return { subfolders: [], resolutions: [], formats: [] };
+  }
 }
